3ca21f70e60ce1ab025879297986f04bc20f68c9
clairzed <clairezuliani@gmail.com>
2014-11-10 22:21:39 +0100

    tags:affichage nv tags en admin

--- app/assets/javascripts/admin/tags.js.coffee
+++ app/assets/javascripts/admin/tags.js.coffee
@@ -1,10 +1,7 @@
 class @Tags
 
   @init = ->
-    console.log 'hey'
     $("[data-tags]").on 'click', ->
-      console.log $(@).data('tags')
-      console.log $("#tag_title").val()
       $.ajax $(@).data("path"),
         data:
           tag_title: $("#tag_title").val(),
@@ -12,7 +9,8 @@ class @Tags
         type: "put",
         dataType: "html",
         success: (html) ->
-          $("[data-tags]").append(html)
+          $("[data-tags='list']").html(html)
+          $("#tag_title").val('')
         error: ->
           alert("Une erreur est intervenue")
 
--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -4,8 +4,12 @@ class Admin::TagsController < Admin::BaseController
     tag = Tag.where(title: params[:tag_title]).first_or_create
     # raise params[:project_id].inspect
     project = Project.find params[:project_id]
-    project.project_tags.create(tag_id: tag.id)
-    # raise project.inspect
+    if project.project_tags.create(tag_id: tag.id)
+      render partial: "admin/projects/tags_list", locals: {tags: project.tags}
+    else
+      flash[:error] = "Une erreur s'est produite lors de la création du projet"
+      render nothing: true
+    end
   end
 
 end
