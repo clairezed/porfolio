7baf3bf32ded464cf651495c03f31594fb346c01
clairezed <clairezuliani@gmail.com>
2014-11-17 22:23:02 +0100

    design show project page

--- app/models/asset/project_picture.rb
+++ app/models/asset/project_picture.rb
@@ -6,9 +6,9 @@ class Asset::ProjectPicture < Asset
     styles: {
       thumb: "30x30#",
       thumb_detail: "100x100>",
-      list: "400x275",
+      list: "400x275#",
       detail: "250",
-      large: "800"
+      large: "800#"
     },
     url: "/uploads/projects/:assetable_id/pictures/:id/:style.:extension",
     path: ":rails_root/public/uploads/projects/:assetable_id/pictures/:id/:style.:extension"
